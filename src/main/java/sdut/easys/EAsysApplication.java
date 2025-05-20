d98a60ae000a7bfeb5251bcb75002fed66ef2c84
111 <852747830@qq.com>
2025-05-20 15:16:11 +0800

    初始化

---
+++ src/main/java/sdut/easys/EAsysApplication.java
@@ -0,0 +1,13 @@
+package sdut.easys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EAsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sdut/easys/EAsysApplicationTests.java
@@ -0,0 +1,13 @@
+package sdut.easys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
